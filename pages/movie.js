290a1f36e8b05b6a3b2f97b69a1c43620d6bb9fb
FUNco <crispy.alc@gmail.com>
2022-09-23 15:23:40 +0900

    FEAT connect movie API

--- pages/movie.js
+++ pages/movie.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Seo from "./components/seo";
 
 export default function Movie() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          "https://api.themoviedb.org/3/movie/popular?api_key=66dca24d08fd3a306a6425e77a96eb22"
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <main className={styles.main}>
       <Seo title="Home" />
-      <h1>movies</h1>
+      {!movies
+        ? null
+        : movies.map((movie) => (
+            <div key={movie.id}>
+              <h3>{movie.original_title}</h3>
+            </div>
+          ))}
     </main>
   );
 }
